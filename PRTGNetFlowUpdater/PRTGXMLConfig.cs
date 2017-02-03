f2205cebcd8060609c1fc483304360aa0e0d517c
Jeremy Gibbons <jeremy.gibbons@laposte.net>
2017-02-03 22:06:42 +0100

    Working treeview build

--- PRTGNetFlowUpdater/PRTGXMLConfig.cs
+++ PRTGNetFlowUpdater/PRTGXMLConfig.cs
@@ -1,13 +1,15 @@
 ﻿
 namespace PRTGNetFlowUpdater
 {
-    using System.Xml;
+    using System.Collections.Generic;
     using System.Xml.Linq;
     using System.Windows.Forms;
 
     class PRTGXMLConfig
     {
         private string configFileName;
+
+        private Dictionary<string, TreeNode> flowChannels = new Dictionary<string, TreeNode>();
 
         public PRTGXMLConfig()
         {
@@ -34,71 +36,48 @@ namespace PRTGNetFlowUpdater
                 return;
             }
 
-            TreeNode currentNode = null;
-
             XDocument xdoc = XDocument.Load(this.ConfigFileName);
 
             foreach (XElement xel in xdoc.Root.Element("basenode").Element("nodes").Element("group").Element("nodes").Elements("probenode"))
             {
                 ParseProbeNode(nodes, xel);
             }
-
-            /*
-            using (XmlReader reader = XmlReader.Create(this.ConfigFileName))
-            {
-                nodes.Clear();
-                reader.MoveToContent();
-
-                if(reader.NodeType == XmlNodeType.Element && reader.Name == "root")
-                {
-                    currentNode = nodes.Add("root");
-                }
-                else
-                {
-                    return;
-                }
-
-                reader.ReadToDescendant("basenode");
-                reader.ReadToDescendant("nodes");
-                reader.ReadToDescendant("group");
-                reader.ReadToDescendant("nodes");
-
-                reader.ReadToDescendant("probenode");
-                XmlReader tempReader = reader.ReadSubtree();
-                tempReader.ReadToDescendant("data");
-                tempReader.ReadToDescendant("name");
-                currentNode.Nodes.Add(tempReader.ReadElementContentAsString());
-
-
-            }
-            */
         }
 
-        void ParseProbeNode(TreeNodeCollection nodes, XElement probeXEl)
+        int ParseProbeNode(TreeNodeCollection nodes, XElement probeXEl)
         {
-            TreeNode probeNode = nodes.Add(((string)probeXEl.Element("data").Element("name")).Trim());
+            TreeNode probeNode = new TreeNode(((string)probeXEl.Element("data").Element("name")).Trim());
 
-            foreach (XElement deviceXEl in probeXEl.Elements("device"))
+            int numChildren = 0;
+
+            foreach (XElement deviceXEl in probeXEl.Element("nodes").Elements("device"))
             {
-                ParseDeviceNode(probeNode.Nodes, deviceXEl);
+                numChildren += ParseDeviceNode(probeNode.Nodes, deviceXEl);
             }
 
-            foreach (XElement groupXEl in probeXEl.Elements("group"))
+            foreach (XElement groupXEl in probeXEl.Element("nodes").Elements("group"))
             {
-                ParseGroupNode(probeNode.Nodes, groupXEl);
+                numChildren += ParseGroupNode(probeNode.Nodes, groupXEl);
             }
+
+            if(numChildren > 0)
+            {
+                nodes.Add(probeNode);
+            }
+
+            return numChildren;
         }
 
         int ParseDeviceNode(TreeNodeCollection nodes, XElement deviceXEl)
         {
-            TreeNode deviceNode = new TreeNode((string)deviceXEl.Element("data").Element("name"));
+            TreeNode deviceNode = new TreeNode(((string)deviceXEl.Element("data").Element("name")).Trim());
             int netFlowSensorCount = 0;
             foreach (XElement sensorXEl in deviceXEl.Element("nodes").Elements("sensor"))
             {
-                if(((string)sensorXEl.Element("sensorkind")).Equals("netflow9custom"))
+                if(((string)sensorXEl.Element("data").Element("sensorkind")).Trim().Equals("netflow9custom"))
                 {
                     netFlowSensorCount++;
-                    deviceNode.Nodes.Add((string)sensorXEl.Element("flowchannel"));
+                    deviceNode.Nodes.Add(((string)sensorXEl.Element("data").Element("flowchannel")).Trim());
                 }
             }
 
@@ -110,12 +89,28 @@ namespace PRTGNetFlowUpdater
             return netFlowSensorCount;
         }
 
-        void ParseGroupNode(TreeNodeCollection nodes, XElement groupXEl)
+        int ParseGroupNode(TreeNodeCollection nodes, XElement groupXEl)
         {
-            TreeNode groupNode = new TreeNode((string)groupXEl.Element("data").Element("name"));
+            TreeNode groupNode = new TreeNode(((string)groupXEl.Element("data").Element("name")).Trim());
 
-            foreach(XElement deviceXEl in groupXEl.)
+            int numChildren = 0;
 
+            foreach (XElement subGroupXEl in groupXEl.Element("nodes").Elements("group"))
+            {
+                numChildren += ParseGroupNode(groupNode.Nodes, subGroupXEl);
+            }
+
+            foreach (XElement deviceXEl in groupXEl.Element("nodes").Elements("device"))
+            {
+                numChildren += ParseDeviceNode(groupNode.Nodes, deviceXEl);
+            }
+
+            if(numChildren > 0)
+            {
+                nodes.Add(groupNode);
+            }
+
+            return numChildren;
         }
     }
 }
